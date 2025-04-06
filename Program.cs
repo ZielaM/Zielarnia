4d36b395fa6f87ba718ceba4a96e78454ade7297
ZielaM <igor.zielinski@uczen.zsk.poznan.pl>
2025-04-06 15:12:20 +0200

    Połączenie z bazą danych

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;
 
 namespace Zielarnia
 {
@@ -10,6 +11,18 @@ namespace Zielarnia
     {
         static void Main(string[] args)
         {
+            var connectionString = "Server=localhost;Database=zielonazielarnia;User=root;Password=admin;";
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var command = new MySqlCommand("SELECT * FROM klienci;", connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetInt32(0)); // przykład odczytu pierwszej kolumny
+            }
         }
     }
 }
